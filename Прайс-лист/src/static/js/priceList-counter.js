b7f87dca8726c073c6b0991ebfdc0609f9ec06a1
a.merkulov <merkuller26@gmail.com>
2021-05-05 16:02:11 +0500

    Задача № 73539. Готовый счётчик.

--- Прайс-лист/src/static/js/priceList-counter.js
+++ Прайс-лист/src/static/js/priceList-counter.js
@@ -9,6 +9,7 @@ if (document.querySelector('.price-list')) {
         if (e.target.classList.contains('card__plus-btn')) {
             e.preventDefault();
             let field = e.target.parentElement.querySelector('.card__field');
+            field.parentElement.querySelector('.card__minus-btn').style.pointerEvents = 'auto';
             let shopBasketEl = field.parentElement.nextElementSibling;
             field.value ++;
             shopBasketEl.classList.add('change-position');
@@ -17,12 +18,16 @@ if (document.querySelector('.price-list')) {
             e.preventDefault();
             let field = e.target.parentElement.querySelector('.card__field');
             field.value --;
+            if (field.value === '0') e.target.style.pointerEvents = 'none';
         }
     })
 
     contentBlock.addEventListener('input', e => {
         if (e.target.classList.contains('card__field')) {
             e.target.parentElement.nextElementSibling.classList.add('change-position'); 
-        } 
+        }
+        if (e.target.classList.contains('card__field')) {
+            e.target.value === '' || e.target.value === '0' ? e.target.previousElementSibling.style.pointerEvents = 'none' : e.target.previousElementSibling.style.pointerEvents = 'auto';
+        }  
     })
 }
